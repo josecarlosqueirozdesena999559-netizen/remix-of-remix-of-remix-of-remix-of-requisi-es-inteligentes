c54f0e62d144670ec78648f94ce16e1e315f4c13
Jose Carlos <josecarlosqueirozdesena999559@gmail.com>
2026-05-07 09:12:44 -0300

    Configura deploy na Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
@@ -12,7 +12,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    nitro(),
     react(),
-    cloudflare(),
   ],
 });
